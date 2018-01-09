399eceb1ad08c35f541a82c4e1e9b056fb03b6a0
Maria Baker <maria.baker.bsee@outlook.com>
2018-01-09 18:26:44 -0500

    Twitter and Spotify not working.

--- liri.js
+++ liri.js
@@ -2,6 +2,7 @@
 var fs = require('fs');
 var keys = require('../liri-node-app/keys');
 var spotify = require('spotify');
+var request = require('request');
 //Executed (npm install twitter) in node.
 //Node require command to access twitter.
 var Twitter = require('twitter');
@@ -10,25 +11,26 @@ var params = {
     count: 2
     };
 
-//var client = new Twitter(keys.twitterKeys);
+var client = new Twitter(keys.twitterKeys);
 
 //Get request for MBAlias6265 twitter account.
-//client.get('statuses/user_timeline', params, function(error, tweets, response) {
-    //console.log(error);
-    //console.log(tweets);
-//});
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    console.log(error);
+    console.log(tweets);
+});
 //==========================================================================
   
 var client = new spotify ({
-    SPOTIFY_ID:'7b3868ab18ac4fe79809e7852f14565f',
-    SPOTIFY_SECRET:'6f3155929e894477a9598d727980b02f',
+    id:'7b3868ab18ac4fe79809e7852f14565f',
+    secret:'6f3155929e894477a9598d727980b02f'
+})
 
-    spotify:search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
+    client.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
     if ( err ) {
     console.log('Error occurred: ' + err);
       return;
-   }
+   };
 
-    // Do something with 'data'
+    //Do something with 'data'
     console.log(data);
 });
