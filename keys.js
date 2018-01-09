f2811785b03ec6800a0e6e0378d912350764c613
Maria Baker <maria.baker.bsee@outlook.com>
2018-01-09 10:54:43 -0500

    Removed space before first twitter key.

--- keys.js
+++ keys.js
@@ -1,7 +1,7 @@
 console.log('this is loaded');
 
 var twitterKeys = {
-  consumer_key: '	HcGOzBp538yI2PPF8F8ieA2n6',
+  consumer_key: 'HcGOzBp538yI2PPF8F8ieA2n6',
   consumer_secret: 'yeAyWREqq9a6Gs84CYbFf5KRshShxhstDuC4DeA9Imsw14WUh3',
   access_token_key: '946906165060997120-jwD8wb1G0sFu12BgR2GMwFyBA2XN6CJ',
   access_token_secret: 'sGCxzOUqexDq0DlacNkEqIoynKCPFypxBV88QrtKQa0Qy',
@@ -9,10 +9,10 @@ var twitterKeys = {
 
 module.exports = twitterKeys;
 
-//var spotifyKeys = {
+var spotifyKeys = {
 
- // SPOTIFY_ID:7b3868ab18ac4fe79809e7852f14565f,
-  //SPOTIFY_SECRET:6f3155929e894477a9598d727980b02f,
-//}
+SPOTIFY_ID: '7b3868ab18ac4fe79809e7852f14565f',
+SPOTIFY_SECRET: '6f3155929e894477a9598d727980b02f',
+}
 
-//module.exports = spotifyKeys;
+module.exports = spotifyKeys;
--- liri.js
+++ liri.js
@@ -1,35 +1,34 @@
-var $ = require('jquery'),
-request = require('request'),
-fs = require('fs'),
-keys = require('../liri-node-app/keys'),
-//var spotify = require('spotify');
+request = require('request');
+fs = require('fs');
+var keys = require('../liri-node-app/keys');
+var spotify = require('spotify');
 //Executed (npm install twitter) in node.
 //Node require command to access twitter.
-Twitter = require('twitter'),
+var Twitter = require('twitter');
 params = {
     screen_name: 'MBAlias6265',
     count: 2
     }
 
-    client = new Twitter(keys.twitterKeys);
+client = new Twitter(keys.twitterKeys);
 
 //Get request for MBAlias6265 twitter account.
-    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
     console.log(error);
     console.log(tweets);
 });
 
   
-//var clientTwo = new Spotify({
+//var client = new spotify ({
     //SPOTIFY_ID:'7b3868ab18ac4fe79809e7852f14565f',
     //SPOTIFY_SECRET:'6f3155929e894477a9598d727980b02f',
-//});
 //spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
-    //if ( err ) {
-        //console.log('Error occurred: ' + err);
-       // return;
-    //}
+ // if ( err ) {
+    //console.log('Error occurred: ' + err);
+      //return;
+   //}
 
     // Do something with 'data'
     //console.log(data);
 //});
+//-------------------------------------------------------------------
