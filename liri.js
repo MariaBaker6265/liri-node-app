af1d6dc6669ce859582387939459db307ea5bddd
Maria Baker <maria.baker.bsee@outlook.com>
2018-01-09 13:44:48 -0500

    Most recent attempt.

--- liri.js
+++ liri.js
@@ -1,34 +1,34 @@
-request = require('request');
-fs = require('fs');
+'use strict';
+var fs = require('fs');
 var keys = require('../liri-node-app/keys');
 var spotify = require('spotify');
 //Executed (npm install twitter) in node.
 //Node require command to access twitter.
 var Twitter = require('twitter');
-params = {
+var params = {
     screen_name: 'MBAlias6265',
     count: 2
-    }
+    };
 
-client = new Twitter(keys.twitterKeys);
+//var client = new Twitter(keys.twitterKeys);
 
 //Get request for MBAlias6265 twitter account.
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-    console.log(error);
-    console.log(tweets);
-});
-
+//client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    //console.log(error);
+    //console.log(tweets);
+//});
+//==========================================================================
   
-//var client = new spotify ({
-    //SPOTIFY_ID:'7b3868ab18ac4fe79809e7852f14565f',
-    //SPOTIFY_SECRET:'6f3155929e894477a9598d727980b02f',
-//spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
- // if ( err ) {
-    //console.log('Error occurred: ' + err);
-      //return;
-   //}
+var client = new spotify ({
+    SPOTIFY_ID:'7b3868ab18ac4fe79809e7852f14565f',
+    SPOTIFY_SECRET:'6f3155929e894477a9598d727980b02f',
+
+    spotify:search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
+    if ( err ) {
+    console.log('Error occurred: ' + err);
+      return;
+   }
 
     // Do something with 'data'
-    //console.log(data);
-//});
-//-------------------------------------------------------------------
+    console.log(data);
+});
