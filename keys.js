4ae1cc2d3baddc5e92aa01a93b55b53bea85a391
Maria Baker <maria.baker.bsee@outlook.com>
2018-01-08 23:24:09 -0500

    Attempt to get twitter working.

--- keys.js
+++ keys.js
@@ -2,10 +2,18 @@ console.log('this is loaded');
 
 var twitterKeys = {
   //removed single quotes
-  consumer_key: 	S45e15IagMWShQS4UoB1MpaJq,
-  consumer_secret: FuI8F2Rpk5TM0QgQAEDcgQoPCYfxrOL6CILXPjAmfKuLgqdPyR,
-  access_token_key: 946906165060997120-UurM0sNtpKBskDk2nM6d29bGjCke9N3,
-  access_token_secret: 3J8w4zchTggUS0g3sWxil5zkoNKri2imXJcZjY4eb9Ckt,
+  consumer_key: 'S45e15IagMWShQS4UoB1MpaJq',
+  consumer_secret: 'FuI8F2Rpk5TM0QgQAEDcgQoPCYfxrOL6CILXPjAmfKuLgqdPyR',
+  access_token_key: '946906165060997120-UurM0sNtpKBskDk2nM6d29bGjCke9N3',
+  access_token_secret: '3J8w4zchTggUS0g3sWxil5zkoNKri2imXJcZjY4eb9Ckt',
 }
 
 module.exports = twitterKeys;
+
+//var spotifyKeys = {
+
+ // SPOTIFY_ID:7b3868ab18ac4fe79809e7852f14565f,
+  //SPOTIFY_SECRET:6f3155929e894477a9598d727980b02f,
+//}
+
+//module.exports = spotifyKeys;
--- liri.js
+++ liri.js
@@ -1,23 +1,34 @@
 
 //require('dotenv').config({path: 'C:\Users\RetailAdmin\Documents\CWRUCodingBootcamp\Homeworks\Homework10\liri-node-app\.env'})
-require('dotenv').config()
+//require('dotenv').config()
 var $ = require('jquery');
-
+var request = require('request');
+var fs = require('fs');
+var keys = require('../liri-node-app/keys');
+var Twitter = require('twitter');
+var client = new Twitter(keys.twitterKeys);
+//var spotify = require('spotify');
 //Executed (npm install twitter) in node.
 //Node require command to access twitter.
-var Twitter = require('twitter');
-
-var client = new Twitter({
-    consumer_key: process.env.twitter_consumer_key,
-    consumer_secret: process.env.twitter_consumer_secret,
-    access_token_key: process.env.twitter_access_token_key,
-    access_token_secret: process.env.twitter_access_token_secret
-  });
 
 
+
+//Get request for MBAlias6265 twitter account.
   client.get('search/tweets', {q: 'node.js'}, function(error, tweets, response) {
     console.log(tweets);
  });
 
   
-//var spotify = new Spotify(keys.spotify);
+//var clientTwo = new Spotify({
+    //SPOTIFY_ID:'7b3868ab18ac4fe79809e7852f14565f',
+    //SPOTIFY_SECRET:'6f3155929e894477a9598d727980b02f',
+//});
+//spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
+    //if ( err ) {
+        //console.log('Error occurred: ' + err);
+       // return;
+    //}
+
+    // Do something with 'data'
+    //console.log(data);
+//});
